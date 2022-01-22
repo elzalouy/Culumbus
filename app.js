0a7ad50b171507e46e500b08c377602093d09979
Ezat Elzalouy <ezat.elzalouy@o-projects.com>
2022-01-22 21:57:11 +0200

    Push notification

--- app.js
+++ app.js
@@ -108,12 +108,15 @@ app.delete("/delete/:image", function (req, res) {
 });
 
 //&authSource=admin&replicaSet=culumbus-db&tls=true&tlsCAFile=./dbcert.crtc
-// `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
+// "mongodb://localhost:27017/culumbusNew",
 mongoose
-  .connect("mongodb://localhost:27017/culumbusNew", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => {
     console.log("Server running...");
     app.listen(3000);
