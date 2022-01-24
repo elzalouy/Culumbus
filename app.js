25f696f224349a7e6b616a4e84eb7baaaa33617c
Ezat Elzalouy <ezat.elzalouy@o-projects.com>
2022-01-24 20:31:02 +0200

    Push brain db

--- app.js
+++ app.js
@@ -116,8 +116,8 @@ app.delete("/delete/:image", function (req, res) {
 // "mongodb://localhost:27017/culumbusNew",
 mongoose
 .connect(
-  {
-      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
+  `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
+    {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     }
